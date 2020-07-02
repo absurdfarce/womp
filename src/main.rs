1836a9aaf974bde2a0440ee1f61763981d27ca09
Bret McGuire <bret.mcguire@datastax.com>
2020-07-02 03:21:04 -0500

    * Switch to sane methods for searching the iterator * Add clearer output for proc/inode not found * Add explicit return status codes for these cases as well

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use procfs::process::FDInfo;
 use procfs::process::FDTarget;
+use procfs::process::Process;
 
 extern crate procfs;
 
@@ -8,13 +8,16 @@ extern crate procfs;
 //
 // Absolutely nothing.  Except I can never remember it.
 
-fn extract_inode(arg:&FDTarget) -> u32 {
-   if let FDTarget::Socket(inode) = arg { *inode }
+fn extract_inode(target:&FDTarget) -> u32 {
+   if let FDTarget::Socket(inode) = target { *inode }
    else { 0 }
 }
 
-fn contains_inode(fds: &Vec<FDInfo>, target_inode:u32) -> bool {
-   fds.iter().map(|fd| extract_inode(&fd.target)).any(|inode| inode == target_inode)
+fn contains_inode(proc_entry: &Process, target_inode:u32) -> bool {
+   if let Ok(fds) = proc_entry.fd() {
+      fds.iter().map(|fd| extract_inode(&fd.target)).any(|inode| inode == target_inode)
+   }
+   else { false }
 }
 
 fn main() {
@@ -23,10 +26,23 @@ fn main() {
     println!("Searching for port {:?}", port_arg);
 
     let all_tcp_entries = procfs::net::tcp().unwrap();
-    let tcp_entry = all_tcp_entries.iter().filter(|entry| entry.local_address.port() == port_arg).next().unwrap();
+    let tcp_entry = match all_tcp_entries.iter().find(|entry| entry.local_address.port() == port_arg) {
+    	Some(entry) => entry,
+	None => {
+	     println!("Port {:?} appears to be unused", port_arg);
+	     std::process::exit(1);
+	},
+    };
     println!("Found TCP entry: {:?}", tcp_entry);
 
     let all_proc_entries = procfs::process::all_processes().unwrap();
-    let proc_entry = all_proc_entries.iter().filter(|entry| contains_inode(&entry.fd().unwrap(), tcp_entry.inode)).next().unwrap();
-    println!("Found proc entry: {:?}", proc_entry);
+    let proc_entry = match all_proc_entries.iter().find(|entry| contains_inode(&entry, tcp_entry.inode)) {
+    	Some(entry) => entry,
+	None => {
+	     println!("Could not find process that owns socket inode {:?}", tcp_entry.inode);
+	     std::process::exit(2);
+	},
+    };
+    println!("Found process entry: {:?}", proc_entry);
+    println!("cmdline: {:?}", proc_entry.cmdline().unwrap());
 }
