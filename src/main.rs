30cbf745e912072e4766be617e8b589ce1ef34a1
Bret McGuire <bret.mcguire@datastax.com>
2021-05-12 00:12:45 -0500

    Starting to clean some things up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use std::env;
+
+extern crate procfs;
 use procfs::net::TcpNetEntry;
+use procfs::process::FDInfo;
 use procfs::process::FDTarget;
 use procfs::process::Process;
 
-extern crate procfs;
-
 extern crate partial_application;
 use partial_application::partial;
-
-// What's wrong with, say, "netstat -tlp4 --numeric-ports"?
-//
-// Absolutely nothing.  Except I can never remember it.
-
-fn find_tcp_entry_by_port(entries:Vec<TcpNetEntry>, port_to_find:u16) -> Option<TcpNetEntry> {
-    entries.into_iter().find(|entry| entry.local_address.port() == port_to_find)
-}
 
 enum ProcSearchResults {
     Found(Process),
@@ -22,6 +15,13 @@ enum ProcSearchResults {
     // We were able to search the file descriptors for every process but somehow didn't find the
     // expected inode.  Logically possible but hard to imagine how this would happen in practice.
     MissingSearchedAll,
+}
+
+fn contains_inode(info:&FDInfo, inode_to_find:u32) -> bool {
+    match info.target {
+        FDTarget::Socket(inode) => inode == inode_to_find,
+        _ => false
+    }
 }
 
 fn find_proc_entry_by_inode(entries:Vec<Process>, inode_to_find:u32) -> ProcSearchResults {
@@ -34,12 +34,7 @@ fn find_proc_entry_by_inode(entries:Vec<Process>, inode_to_find:u32) -> ProcSear
                 false
             },
             Ok(fds) => {
-                fds.iter().any(|fd| {
-                    match fd.target {
-                        FDTarget::Socket(inode) => inode == inode_to_find,
-                        _ => false
-                    }
-                })
+                fds.iter().any(|fdinfo| { contains_inode(fdinfo, inode_to_find) })
             }
         }
     }) {
@@ -48,29 +43,37 @@ fn find_proc_entry_by_inode(entries:Vec<Process>, inode_to_find:u32) -> ProcSear
     }
 }
 
+fn find_tcp_entry(port_to_find:u16, entries:Vec<TcpNetEntry>) -> Option<TcpNetEntry> {
+    entries.into_iter().find(|entry| entry.local_address.port() == port_to_find)
+}
+
+fn find_global_tcp_entry(port_to_find:u16) -> Result<TcpNetEntry, String> {
+
+    match procfs::net::tcp() {
+
+        Err(e) => Err(format!("Error retrieving TCP entries: {:?}", e)),
+        Ok(entries) => match find_tcp_entry(port_to_find, entries) {
+            None => Err(format!("Port {:?} appears to be unused", port_to_find)),
+            Some(entry) => Ok(entry)
+        }
+    }
+}
+
 fn main() {
     let arg: Option<String> = env::args().skip(1).next();
     let port_arg = arg.map(|s| s.to_string().parse::<u16>().unwrap()).unwrap();
     println!("Searching for port {:?}", port_arg);
 
-    let tcp_entry = match procfs::net::tcp()
-        .map(partial!(find_tcp_entry_by_port => _, port_arg)) {
-
-        Err(e) => {
-            println!("Error retrieving TCP entries: {:?}", e);
-	    std::process::exit(2);
-        },
-            Ok(opt) => match opt {
-                None => {
-	            println!("Port {:?} appears to be unused", port_arg);
-                    std::process::exit(1);
-                },
-                Some(entry) => entry
-            }
-	};
+    let tcp_entry = match find_global_tcp_entry(port_arg) {
+        Err(errstr) => {
+            println!("{}", errstr);
+            std::process::exit(1);
+        }
+        Ok(entry) => entry
+    };
     println!("Found TCP entry: {:?}", tcp_entry);
-
     let inode = tcp_entry.inode;
+
     let proc_entry = match procfs::process::all_processes()
         .map(partial!(find_proc_entry_by_inode => _, inode)) {
 
@@ -78,12 +81,12 @@ fn main() {
             println!("Error retrieving process entries: {:?}", e);
             std::process::exit(3);
         },
-            Ok(ProcSearchResults::Found(entry)) => entry,
-            Ok(ProcSearchResults::MissingSearchedSome) => {
+        Ok(ProcSearchResults::Found(entry)) => entry,
+        Ok(ProcSearchResults::MissingSearchedSome) => {
 	        println!("Could not find process that owns socket inode {:?}.  Could not search all processes; consult stdout for additional info", inode);
 	        std::process::exit(4);
-            },
-            Ok(ProcSearchResults::MissingSearchedAll) => {
+        },
+        Ok(ProcSearchResults::MissingSearchedAll) => {
 	        println!("Could not find process that owns socket inode {:?}.  We searched all processes... and that's kinda weird", inode);
 	        std::process::exit(5);
             }
